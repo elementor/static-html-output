f50245d8e0960c7392b096176f94d08a7f6033c3
Leon <me@ljs.dev>
2020-05-17 18:13:06 +0930

    rename BitBucket for psr;update build tools

--- tools/build_release.sh
+++ tools/build_release.sh
@@ -1,40 +1,57 @@
 #!/bin/bash
 
-# run from project root
+######################################
+##
+## Build WP2Static Boilerplate Addon
+##
+## script archive_name dont_minify
+##
+## places archive in $HOME/Downloads
+##
+######################################
 
+# run script from project root
 EXEC_DIR=$(pwd)
-# give argument for name of zip to be created, ie user-leon-test-new-function
 
 TMP_DIR=$HOME/plugintmp
+rm -Rf $TMP_DIR
 mkdir -p $TMP_DIR
 
 rm -Rf $TMP_DIR/static-html-output-plugin
 mkdir $TMP_DIR/static-html-output-plugin
 
 
-cp -r $EXEC_DIR/languages $TMP_DIR/static-html-output-plugin/
-cp -r $EXEC_DIR/plugin $TMP_DIR/static-html-output-plugin/
-cp -r $EXEC_DIR/readme.txt $TMP_DIR/static-html-output-plugin/
+# clear dev dependencies
+rm -Rf $EXEC_DIR/vendor/*
+# load prod deps and optimize loader
+composer install --no-dev --optimize-autoloader
+
+
+# cp all required sources to build dir
+cp -r $EXEC_DIR/*.php $TMP_DIR/static-html-output-plugin/
+cp -r $EXEC_DIR/src $TMP_DIR/static-html-output-plugin/
+cp -r $EXEC_DIR/vendor $TMP_DIR/static-html-output-plugin/
+cp -r $EXEC_DIR/README.txt $TMP_DIR/static-html-output-plugin/
 cp -r $EXEC_DIR/views $TMP_DIR/static-html-output-plugin/
-cp -r $EXEC_DIR/statichtmloutput.php $TMP_DIR/static-html-output-plugin/
-cp -r $EXEC_DIR/statichtmloutput.css $TMP_DIR/static-html-output-plugin/
+cp -r $EXEC_DIR/wpassets $TMP_DIR/static-html-output-plugin/
 
 cd $TMP_DIR
-
-rm static-html-output-plugin/plugin/.htaccess
 
 # tidy permissions
 find . -type d -exec chmod 755 {} \;
 find . -type f -exec chmod 644 {} \;
 
-# strip comments and whitespace from each PHP file
-
-if [ -z "$2" ]; then
-  find .  ! -name 'statichtmloutput.php' -name \*.php -exec $EXEC_DIR/tools/compress_php_file {} \;
-fi
-
 zip -r -9 ./$1.zip ./static-html-output-plugin
 
 cd -
 
+mkdir -p $HOME/Downloads/
+
 cp $TMP_DIR/$1.zip $HOME/Downloads/
+
+# reset dev dependencies
+cd $EXEC_DIR
+# clear dev dependencies
+rm -Rf $EXEC_DIR/vendor/*
+# load prod deps
+composer install
